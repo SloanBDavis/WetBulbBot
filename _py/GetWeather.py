2cc264b0b1dbb24871cd703b61a919c474545e04
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2022-05-17 17:40:09 -0500

    implemented calcWBT and needsTweet

--- _py/GetWeather.py
+++ _py/GetWeather.py
@@ -5,8 +5,10 @@
 #for that city
 
 #that data is used to build a tweet
+from operator import truediv
 import requests
 import json
+import math
 
 class GetWeather():
     
@@ -28,14 +30,23 @@ class GetWeather():
         cityWeather = requests.get("https://api.openweathermap.org/data/2.5/weather?units=metric&lat={}&lon={}&appid={}".format(lat, lon, self.key))
         cityData = cityWeather.json()
 
-    def calcWBT(self) -> float:
-        pass
+    def calcWBT(self, temp, relHumidity) -> float:
+        #formula is Tw = T * arctan[0.151977 * (rh% + 8.313659)^(1/2)] + arctan(T + rh%) - arctan(rh% - 1.676331) + 0.00391838 *(rh%)^(3/2) * arctan(0.023101 * rh%) - 4.686035
+        #                    1                                           2                 3                                                  4
+        arctan1 = math.atan( 0.151977 * ( (relHumidity + 8.313659) ** (1/2) ) )
+        arctan2 = math.atan(temp + relHumidity)
+        arctan3 = math.atan(relHumidity - 1.676331)
+        arctan4 = math.atan(0.023101 * relHumidity)
 
-    def calcWBGT(self) -> float:
-        pass
+        wbt = temp * arctan1 + arctan2 - arctan3 + ( 0.00391838 *(relHumidity) ** (3/2) ) * arctan4 - 4.686035
 
-    def needsTweet(self) -> bool:
-        pass
+        return wbt
+
+    def needsTweet(self, wbt) -> bool:
+        if(wbt >= 32):
+            return True
+        return False
+
 
     
 
